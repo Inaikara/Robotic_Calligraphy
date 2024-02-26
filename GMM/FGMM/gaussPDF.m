1de8eabf67120e1f2c79104651d62eb25bce9cf4
Inaikara <frhyujk@outlook.com>
2024-02-26 10:17:58 +0800

    做了些修改

--- GMM/FGMM/gaussPDF.m
+++ GMM/FGMM/gaussPDF.m
@@ -18,5 +18,5 @@ function prob = gaussPDF(Data, Mu, Sigma)
 [nbVar,nbData] = size(Data);
 
 Data = Data' - repmat(Mu',nbData,1);
-prob = sum((Data*inv(Sigma)).*Data, 2);
+prob = sum((Data*inv(Sigma)).*Data,2);
 prob = exp(-0.5*prob) / sqrt((2*pi)^nbVar * (abs(det(Sigma))+realmin));
